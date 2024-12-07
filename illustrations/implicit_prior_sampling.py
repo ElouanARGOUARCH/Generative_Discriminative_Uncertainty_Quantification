44a76dfccf9c1265bbbc2ab5566a2814d8bcb83d
EA264728 <elouan.argouarch@gmail.com>
2024-12-07 15:03:55 +0100

    fixed figures

--- illustrations/implicit_prior_sampling.py
+++ illustrations/implicit_prior_sampling.py
@@ -1,8 +1,10 @@
 from models import *
 from matplotlib.ticker import MaxNLocator
+from matplotlib.gridspec import GridSpec
+
 scale_sigma2 = torch.tensor(.5)
 shape_sigma2 = torch.tensor(.5)
-sigma20 = torch.tensor(1) / 40
+sigma20 = torch.tensor(1)/20
 
 mu_beta = torch.zeros(2)
 Sigma_beta = torch.eye(2)
@@ -27,7 +29,7 @@ sigma2_Y0 = sigma2_X0 * alpha[0] ** 2 + sigma20
 y_marginal = torch.distributions.Normal(mu_Y0, torch.sqrt(sigma2_Y0))
 
 model = discriminative_bayesian_affine_regression()
-number_MC_samples = 500
+number_MC_samples = 1000
 list_x = []
 for i in range(number_MC_samples):
     x0 = inference_prior.sample()
@@ -35,8 +37,9 @@ for i in range(number_MC_samples):
     x, _, _ = model.sample_x0_given_y0_D_Y_gibbs(y0, DX, DY)
     list_x.append(x[-10:])
 
-fig = plt.figure(figsize=(20, 6))
-ax = fig.add_subplot(111)
+fig = plt.figure(figsize=(20,10))
+gs = GridSpec(2,2, figure = fig, width_ratios=[3,2], height_ratios=[1,1])
+ax = fig.add_subplot(gs[0,0])
 tt = torch.linspace(0, 6, 300)
 ax.set_xlim(0, 6)
 ax.set_ylim(0, 5)
@@ -66,10 +69,10 @@ ax.plot(torch.exp(y_marginal.log_prob(tt_y)), tt_y, linestyle='--', color='green
 ax.plot(tt, torch.exp(prior_dataset.log_prob(tt)), linestyle='--', label=r'$\mathcal{P}_{X}^{\mathcal{D}}$', color='C1')
 ax.legend(ncol=1, fontsize=20, loc='upper right')
 
-plt.subplots_adjust(wspace=0.05, hspace=0)
-plt.show()
-fig = plt.figure(figsize=(20, 6))
-ax = fig.add_subplot(111)
+#plt.subplots_adjust(wspace=0.05, hspace=0)
+#plt.show()
+#fig = plt.figure(figsize=(20, 6))
+ax = fig.add_subplot(gs[1,0])
 ax.set_xlim(0, 6)
 ax.set_ylim(0, 5)
 ax.scatter(DX.numpy(), DY.numpy(), alpha=.5, label=r'$\mathcal{D}=\{(x_i,y_i)\}$', color='C1')
@@ -83,16 +86,20 @@ ax.legend(ncol=1, fontsize=20, loc='upper right')
 ax.xaxis.set_major_locator(MaxNLocator(integer=True))
 ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 
-plt.show()
-fig = plt.figure(figsize=(12, 12))
-ax = fig.add_subplot(111)
+#plt.show()
+#fig = plt.figure(figsize=(12, 12))
+#ax = fig.add_subplot(111)
+
+ax = fig.add_subplot(gs[:,1])
 ax.plot(tt, torch.exp(prior_dataset.log_prob(tt)), linestyle='--', label=r'$\mathcal{P}_{X}^{\mathcal{D}}$', color='C1')
 ax.hist(torch.cat(list_x).numpy(), bins=60, density=True, histtype='step', label=r'$p(x_0|\mathcal{D})$ dis.',
         color='C9')
-ax.plot(tt, torch.exp(inference_prior.log_prob(tt.unsqueeze(-1))), color='green', label=r'prior $\Pi_{X_0}$')
+ax.plot(tt, torch.exp(inference_prior.log_prob(tt.unsqueeze(-1))), color='green', label=r'prior $\pi_{X_0}$')
 
 ax.legend(ncol=1, fontsize=16.6, loc='upper left')
 ax.set_xlim(0, 6)
 ax.xaxis.set_major_locator(MaxNLocator(integer=True))
 ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+fig.tight_layout()
+fig.savefig('figure.png', dpi = fig.dpi)
 plt.show()
